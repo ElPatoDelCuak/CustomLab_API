8eb1ad8eaec17d5725f29517e1d20e46abca21d6
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-01-29 16:40:39 +0100

    more methods productRepostiroy

--- customlab_models/repositories/productoRepository.py
+++ customlab_models/repositories/productoRepository.py
@@ -14,3 +14,27 @@ class ProductoModel:
             'id_producto', 'precio_venta', 'precio_costo',
             'stock', 'categoria', 'personalizable'
         )
+    
+    def createProducto(self, data):
+        return Productos.objects.create(
+            nombre_producto=data[1],
+            precio_venta=data[2],
+            precio_costo=data[3],
+            stock=data[4],
+            categoria=data[5],
+            personalizable=data[6],
+        )
+
+    def updateProducto(self, idProducto, data):
+        Productos.objects.filter(id_producto=idProducto).update(
+            nombre_producto=data[1],
+            precio_venta=data[2],
+            precio_costo=data[3],
+            stock=data[4],
+            categoria=data[5],
+            personalizable=data[6],
+        )
+        return Productos.objects.get(id_producto=idProducto)
+    
+    def deleteProducto(self, idProducto):
+        Productos.objects.filter(id_producto=idProducto).delete()
