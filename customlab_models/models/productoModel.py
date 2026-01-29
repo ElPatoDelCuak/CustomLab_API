47b4a3701bbb1a8daf4e7b21f66de2e57132382e
Tomardoxx <157169041+Tomardoxx@users.noreply.github.com>
2026-01-29 15:42:29 +0100

    Model Changes getProductoById

--- customlab_models/models/productoModel.py
+++ customlab_models/models/productoModel.py
@@ -1,7 +1,12 @@
 from .core.models import Productos
 class ProductoModel:
-    def getProductos():
+    def getProductos(self):
         return Productos.objects.all().values(
+            'id_producto', 'precio_venta', 'precio_costo',
+        'stock', 'categoria', 'personalizable'
+        )
+    def getProductoById(self, idProducto):
+        return Productos.objects.filter(id_producto=idProducto).values(
             'id_producto', 'precio_venta', 'precio_costo',
         'stock', 'categoria', 'personalizable'
         )
