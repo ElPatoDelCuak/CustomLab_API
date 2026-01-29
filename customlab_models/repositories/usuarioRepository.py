6cd3f508f5cb86b78bab61c21d67b0167673e016
Tomardoxx <157169041+Tomardoxx@users.noreply.github.com>
2026-01-29 17:27:10 +0100

    Changes for user

---
+++ customlab_models/repositories/usuarioRepository.py
@@ -0,0 +1,38 @@
+from .core.models import Usuarios
+class UsuarioRepository:
+    @staticmethod
+    def getUsuarios(self):
+        return Usuarios.objects.all().values(
+            'id_usuario','nombre','apellidos','email','fecha_nacimiento','twofa','rol'
+        )
+    @staticmethod
+    def getUsuarioById(idUsuario):
+        return Usuarios.objects.filter(id_usuario=idUsuario).values(
+            'id_usuario','nombre','apellidos','email','fecha_nacimiento','twofa','rol'
+        )
+    @staticmethod
+    def createUsuario(data):
+        Usuarios.objects.create(
+            nombre=data.get('nombre'),
+            apellidos=data.get('apellidos'),
+            email=data.get('email'),
+            fecha_nacimiento=data.get('fecha_nacimiento'),
+            twofa=data.get('twofa'),
+            rol=data.get('rol'),
+        )
+        return Usuarios.objects.last()
+    @staticmethod
+    def updateUsuario(idUsuario, data):
+        Usuarios.objects.filter(id_usuario=idUsuario).update(
+            nombre=data.get('nombre'),
+            apellidos=data.get('apellidos'),
+            email=data.get('email'),
+            fecha_nacimiento=data.get('fecha_nacimiento'),
+            twofa=data.get('twofa'),
+            rol=data.get('rol'),
+        )
+        return Usuarios.objects.get(id_usuario=idUsuario)
+    @staticmethod
+    def deleteUsuario(idUsuario):
+        Usuarios.objects.filter(id_usuario=idUsuario).delete()
+        return Usuarios.objects.get(id_usuario=idUsuario)
