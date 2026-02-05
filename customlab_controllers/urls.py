3b9a71f5bd2f32020d84225f87ce594096c85695
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-02-05 17:15:11 +0100

    Starting incidencias development

--- customlab_controllers/urls.py
+++ customlab_controllers/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .controllers.productoController import getProductos, getProductoById, createProducto, updateProducto, deleteProducto
 from .controllers.usuarioController import getUsuarios, getUsuarioById, createUsuario, updateUsuario, deleteUsuario
 
+from .controllers.incidenciaController import getIncidencias, getIncidenciaById, createIncidencia, updateEstadoIncidencia, deleteIncidencia
+
 urlpatterns = [
     #Productos URLs
     path('productos/', getProductos),
@@ -15,5 +17,16 @@ urlpatterns = [
     path('usuarios/create/', createUsuario),
     path('usuarios/update/<int:id>/', updateUsuario),
     path('usuarios/delete/<int:id>/', deleteUsuario),
-    # Pedido URLs
+    #Pedido URLs
+
+
+
+
+
+    #Incidencias URLs
+    path('incidencias/', getIncidencias),
+    path('incidencias/<int:id>/', getIncidenciaById),
+    path('incidencias/create/', createIncidencia),
+    path('incidencias/update/<int:id>/', updateEstadoIncidencia),
+    path('incidencias/delete/<int:id>/', deleteIncidencia),
 ]
