4a20e278e62c3565ab51381e01598a90947c38f0
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-02-10 17:25:33 +0100

    funcionando con django

--- customlab_models/models.py
+++ customlab_models/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.utils import timezone
 
 
 class AuthGroup(models.Model):
@@ -176,7 +177,7 @@ class Incidencias(models.Model):
     id_incidencia = models.AutoField(primary_key=True)
     id_usuario = models.ForeignKey('Usuarios', models.DO_NOTHING, db_column='id_usuario')
     tipo = models.CharField(max_length=100)
-    fecha = models.DateTimeField(auto_now_add=True)
+    fecha = models.DateTimeField(default=timezone.now)
     estado = models.CharField(max_length=50)
     descripcion = models.TextField()
 
