56a0a2708c7cc055a34a6c104ec4a03a41cedaa6
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-02-10 17:29:39 +0100

    funcionando base de datos

--- customlab_models/models.py
+++ customlab_models/models.py
@@ -6,7 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.utils import timezone
+from django.db.models.functions import Now
 
 
 class AuthGroup(models.Model):
@@ -177,7 +177,7 @@ class Incidencias(models.Model):
     id_incidencia = models.AutoField(primary_key=True)
     id_usuario = models.ForeignKey('Usuarios', models.DO_NOTHING, db_column='id_usuario')
     tipo = models.CharField(max_length=100)
-    fecha = models.DateTimeField(default=timezone.now)
+    fecha = models.DateTimeField(db_default=Now())
     estado = models.CharField(max_length=50)
     descripcion = models.TextField()
 
