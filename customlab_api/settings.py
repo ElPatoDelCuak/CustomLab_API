81accd2cbb96c41e5160a1957385be39b57bf96d
David <194682261+ElPatoDelCuak@users.noreply.github.com>
2026-03-18 22:08:05 +0100

    add cors

--- customlab_api/settings.py
+++ customlab_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
     'rest_framework',
     'customlab_controllers',
     'customlab_models',
@@ -49,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",              # desarrollo local (React)
+    "http://127.0.0.1:3000",              # desarrollo local (React)
+    "https://customlab-ui.vercel.app",    # frontend producción (Vercel)
 ]
 
 ROOT_URLCONF = 'customlab_api.urls'
