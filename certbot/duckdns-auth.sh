8bfa9d7ac00a4101f9f72af99a2cee861ef96cc7
David <194682261+ElPatoDelCuak@users.noreply.github.com>
2026-04-26 11:41:18 +0200

    Update duckdns-auth.sh

--- certbot/duckdns-auth.sh
+++ certbot/duckdns-auth.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Duck DNS Token y Dominio (Se esperan como variables de entorno en el contenedor)
-# Si no están presentes, se pueden hardcodear aquí si se prefiere.
+# Duck DNS Token y Dominio
 TOKEN="${DUCKDNS_TOKEN}"
 DOMAIN="${DUCKDNS_DOMAIN}"
 
-# URL de actualización de Duck DNS para el reto TXT
-# El valor de $CERTBOT_VALIDATION es proporcionado automáticamente por Certbot
-curl -s "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&txt=$CERTBOT_VALIDATION"
+# Usamos wget en lugar de curl porque la imagen de Certbot no tiene curl
+wget -qO- "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&txt=$CERTBOT_VALIDATION"
 
 # Esperar para la propagación DNS
 echo "Esperando 30 segundos para la propagación DNS..."
